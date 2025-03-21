6c6932e93b8402aa090fc4069a32ad1462c2ef33
sanviray <Sanvi.Ray2024@Bitsdesign.edu.in>
2025-03-21 11:25:21 +0530

    Add files via upload

---
+++ flex.py
@@ -0,0 +1,13 @@
+#Flex sensor - Vo = (R^Flex/R+Flex)Vcc, R = 10k
+#Rflex 1  = 26, Rflex 2  =32, R =10k, Vcc = 3.3v
+from machine import ADC, Pin
+import time
+
+flex_sensor = ADC(Pin(4))
+
+flex_sensor.atten(ADC.ATTN_11DB)
+
+while True:
+    sensor_value = flex_sensor.read()
+    print("Flex Sensor Value:", sensor_value)
+    time.sleep(0.5)
